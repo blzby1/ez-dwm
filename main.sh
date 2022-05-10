59e95c56f46ebd4dc9e6f9c461423e1a64e817da
blzby1 <96076743+blzby1@users.noreply.github.com>
2022-05-10 14:48:05 -0500

    comments!!!1!!!

--- main.sh
+++ main.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+# If you somehow don't have bash, please install it. It's a little something guys like me in the tech world call a "dependency"
+# Also, if you get a "command not found" error, then please install that using "sudo pacman -S (command, or package that contains it)"
+# No, I don't know why I'm making this so user-friendly.
+
+
 
